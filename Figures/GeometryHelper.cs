2bf7473e108acbb96ea93867840d31fc629bc933
Kamran Z <kamraneze@code.edu.az>
2023-08-22 10:51:31 +0400

    MAth figures

---
+++ Figures/GeometryHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    internal static class GeometryHelper
+    {
+        //----------------
+        public static int Quadrilateral(int width, int length)
+        {
+            if (width < 0 || length < 0) return 0;
+            return width * length;
+        }
+        //----------------
+        public static int Triangular(int bottomSet, int height)
+        {
+            if (bottomSet < 0 || height < 0) return 0;
+            return (bottomSet * height) / 2;
+        }
+        //----------------
+        public static double Circle(double radius)
+        {
+            if (radius < 0) return 0;
+            double pi = Math.PI;
+            return radius * radius * pi;
+        }
+
+    }
+}
---
+++ Figures/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Figures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quadrilateral = GeometryHelper.Quadrilateral(20, 40);
+            int triangular = GeometryHelper.Triangular(10, 15);
+            double circle = GeometryHelper.Circle(20);
+
+            Console.WriteLine("DordBucaqlinin Sahesi: " + quadrilateral);
+            Console.WriteLine("UcBucaqlinin Sahesi: " + triangular);
+            Console.WriteLine("Dairenin Sahesi: " + circle);
+        }
+    }
+}
